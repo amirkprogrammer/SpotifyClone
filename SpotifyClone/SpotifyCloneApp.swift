ca667f2be3e74c370b04732d05472df47a697c82
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-04-16 19:19:01 -0700

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Amir Kabiri on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
