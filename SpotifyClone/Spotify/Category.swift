df1dbf4259e947421f32d776bcf6b9193d49cc5d
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-04-18 18:19:03 -0700

    - add categories and modify header scroll view

---
+++ SpotifyClone/Spotify/Category.swift
@@ -0,0 +1,12 @@
+//
+//  Category.swift
+//  SpotifyClone
+//
+//  Created by Amir Kabiri on 4/18/24.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable  {
+    case all, music, podcasts, audiobooks
+}
