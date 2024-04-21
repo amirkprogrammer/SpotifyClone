9809314188c41c7bc1841c35fd59d620a1515105
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-04-21 13:50:25 -0700

    - add newRelease Cell component

--- SpotifyClone/Spotify/Core/SpotifyHomeView.swift
+++ SpotifyClone/Spotify/Core/SpotifyHomeView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct SpotifyHomeView: View {
     @State private var currentUser: User? = nil
     @State private var selectedCategory: Category? = nil
+    @State private var products: [Product] = []
     
     var body: some View {
         ZStack {
@@ -18,8 +20,18 @@ struct SpotifyHomeView: View {
             ScrollView(.vertical) {
                 LazyVStack(spacing: 1, pinnedViews: [.sectionHeaders]) {
                     Section {
+                        
+                        VStack(spacing: 16) {
+                            recentsSection
+                            
+                            if let product = products.first {
+                               newReleaseSection(product: product)
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                        
                         ForEach(0..<20) { _ in
-                        Rectangle()
+                            Rectangle()
                                 .fill(.red)
                                 .frame(width: 200, height: 200)
                         }
@@ -42,7 +54,7 @@ struct SpotifyHomeView: View {
     private func getData() async {
         do {
             currentUser = try await DatabaseHelper().getUsers().first
-//            products = try await DatabaseHelper().getProducts()
+            products = try await Array(DatabaseHelper().getProducts().prefix(8))
         } catch {
             
         }
@@ -61,7 +73,7 @@ struct SpotifyHomeView: View {
                 }
             }
             .frame(width: 35, height: 35)
-          
+            
             ScrollView(.horizontal) {
                 HStack(spacing: 8) {
                     ForEach(Category.allCases, id: \.self) { category in
@@ -81,6 +93,33 @@ struct SpotifyHomeView: View {
         .padding(.vertical, 24)
         .padding(.leading, 8)
         .background(.spotifyBlack)
+    }
+    
+    private var recentsSection: some View {
+        NonLazyVGrid(
+            columns: 2,
+            alignment: .center,
+            spacing: 10,
+            items: products) { product in
+                if let product {
+                    SpotifyRecentsCell(
+                        imageName: product.firstImage,
+                        title: product.title
+                    )
+                }
+            }
+    }
+    
+    private func newReleaseSection(product: Product) -> some View {
+        SpotifyNewReleaseCell(
+            imageName: product.firstImage,
+            headline: product.brand,
+            subheadline: product.category,
+            title: product.title,
+            subtitle: product.description,
+            onAddToPlaylistPressed: { },
+            onPlayPressed: { }
+        )
     }
 }
 
